50db3e4cf463f429f951b5845504e15c9cdf6f46
esteban-zh <eszahe302@gmail.com>
2021-07-10 21:18:22 -0500

    suma con prompt

---
+++ suma.js
@@ -0,0 +1,9 @@
+let num1=parseInt(prompt("ingrese el primer numero"));
+let num2=parseInt(prompt("ingrese el segundo numero"));
+let num3=parseInt(prompt("ingrese el tercer numero"));
+
+var sum = (num1 + num2 + num3);
+
+console.log(sum);
+
+document.getElementById("suma").innerHTML=sum;
